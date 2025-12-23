fb32a25d8db266a867ee2e42050bed9c61903d86
thanhan10981 <152773355+thanhan10981@users.noreply.github.com>
2025-12-24 00:19:34 +0700

    update font-end

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
-import POSPage from "../features/pos/POSPage";
+import POS from "../features/pos/POS";
+
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/admin/productSale" />} />
       <Route path="/admin" element={<MainLayout />}>
-        <Route path="productSale" element={<POSPage />} />
+        <Route path="productSale" element={<POS />} />
       </Route>
     </Routes>
   );
