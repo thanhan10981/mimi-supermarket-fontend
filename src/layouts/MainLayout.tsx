25b4f6d51954b8d3eb89268cbe044cfd3a87fffe
Han <ghan42568@gmail.com>
2025-12-24 19:15:35 +0700

    products

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,29 @@
+// MainLayout.tsx
+import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
+import AppHeader from "./AppHeader";
 
-export default function MainLayout() {
+const { Header, Content } = Layout;
+
+export default function MainLayout({ showHeader = true }) {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "#f5f5f5", // nền xám nhạt
-        color: "#000",         // chữ đen
-        padding: 20,
-      }}
-    >
-      <h2>MainLayout OK</h2>
-      <Outlet />
-    </div>
+    <Layout style={{ minHeight: "100vh" }}>
+      {showHeader && (
+        <Header
+          style={{
+            padding: 0,
+            height: 56,        
+            lineHeight: "56px",
+            background: "transparent", 
+          }}
+        >
+          <AppHeader />
+        </Header>
+      )}
+
+      <Content style={{ background: "#f5f5f5" }}>
+        <Outlet />
+      </Content>
+    </Layout>
   );
 }
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,23 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import POSPage from "../features/pos/POSPage";
+import Product from "../features/product/product";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* Trang mặc định */}
       <Route path="/" element={<Navigate to="/admin/productSale" />} />
-      <Route path="/admin" element={<MainLayout />}>
+
+      {/* Layout posSell */}
+      <Route path="/admin" element={<MainLayout showHeader={false}  />}>
+        {/* POS */}
         <Route path="productSale" element={<POSPage />} />
+      </Route>
+
+      <Route path="/admin" element={<MainLayout showHeader/>}>
+        {/* Product / Inventory */}
+        <Route path="products" element={<Product />} />
       </Route>
     </Routes>
   );
